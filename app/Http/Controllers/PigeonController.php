afd9abba9bc5a6008c856ba8746151919db384fe
chr-ge <ace26@protonmail.com>
2020-04-13 18:41:53 -0400

    Organized views\dashboard\pigeon directory

--- app/Http/Controllers/PigeonController.php
+++ app/Http/Controllers/PigeonController.php
@@ -25,22 +25,22 @@ class PigeonController extends Controller
 
     public function users(){
         $users = User::whereNotNull('name')->paginate(10);
-        return view('dashboard.pigeon.users', compact('users'));
+        return view('dashboard.pigeon.users.users', compact('users'));
     }
 
     public function drivers(){
         $drivers = Driver::whereNotNull('name')->paginate(10);
-        return view('dashboard.pigeon.drivers', compact('drivers'));
+        return view('dashboard.pigeon.drivers.drivers', compact('drivers'));
     }
 
     public function restaurants(){
         $restaurants = Restaurant::where('active', true)->paginate(10);
-        return view('dashboard.pigeon.restaurants', compact('restaurants'));
+        return view('dashboard.pigeon.restaurants.restaurants', compact('restaurants'));
     }
 
     public function applications(){
         $restaurants = Restaurant::where('active', false)->whereNull('password')->paginate(10);
-        return view('dashboard.pigeon.applications', compact('restaurants'));
+        return view('dashboard.pigeon.restaurants.applications', compact('restaurants'));
     }
 
     public function settings(){
@@ -49,7 +49,7 @@ class PigeonController extends Controller
     }
 
     public function userDetails(User $user){
-        return view('dashboard.pigeon.u-details', compact('user'));
+        return view('dashboard.pigeon.users.u-details', compact('user'));
     }
 
     public function restaurantDetails(Restaurant $restaurant){
@@ -60,7 +60,7 @@ class PigeonController extends Controller
             return Pigeon::getPercentatgeChange( Menu::newMenuItemsLastMonth($restaurant->id), Menu::newMenuItemsThisMonth($restaurant->id));
         });
 
-        return view('dashboard.pigeon.r-details', compact('restaurant', 'menu_items', 'menu_change'));
+        return view('dashboard.pigeon.restaurants.r-details', compact('restaurant', 'menu_items', 'menu_change'));
     }
 
     public function activateRestaurant(Restaurant $restaurant){
