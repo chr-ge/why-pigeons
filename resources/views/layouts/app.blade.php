77f903c179aff1521c89b3ba03ed2e45e8872207
chr-ge <ace26@protonmail.com>
2020-04-14 21:57:13 -0400

    Fixed jquery link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="http://localhost/why-pigeons/public/dashboard/vendor/jquery/dist/jquery-3.4.1.min.js"></script>
+    <script src="{{ asset('dashboard/vendor/jquery/dist/jquery-3.4.1.min.js') }}"></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
